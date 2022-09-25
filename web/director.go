47f4686445ecd50d81d3a417013cffbfc1a52368
Mohamed Allam <meda3gm@gmail.com>
2022-09-25 12:04:40 +0100

    fix

--- web/director.go
+++ web/director.go
@@ -10,10 +10,12 @@ func NewRouter(b IBuilder) *Director {
 	}
 }
 
+// Build set builder, to the builder we pass ini
 func (d *Director) setBuilder(b IBuilder) {
 	d.builder = b
 }
 
+// Build the routes, takes the port number as a parameter
 func (d *Director) BuildRouter(portNum int) Router {
 	d.builder.setRoutes()
 	d.builder.setPortNum(portNum)
